50f53c20e3a58f64e26c3bfb7c58214ff6cda0a0
Artyom WSL <artyomgv05@gmail.com>
2025-03-01 18:12:36 -0500

    section main

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -9,7 +9,7 @@ export default function Tag({ text, imageSrc }: TagProps) {
   return (
     <div className="flex justify-center items-center my-[10px] rounded-[10px] pl-3 pr-3 h-[37px] bg-[var(--clr-white-10)]">
       <Image src={imageSrc} alt={text} width={16} height={16}/>
-      <span className="text-sm text-white-800 tracking-wider pl-2 font-light">{text}</span>
+      <span className="text-sm tracking-wider pl-2 font-light text-white">{text}</span>
     </div>
   );
 }
