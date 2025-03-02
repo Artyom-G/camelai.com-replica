8cc069b25ba369d7e4498dcc7be8f5206f2a78fa
Artyom WSL <artyomgv05@gmail.com>
2025-03-01 22:40:02 -0500

    deploying navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,27 +1,25 @@
 "use client";
 
-import Link from "next/link";
-
 export default function Navbar() {
   return (
     <nav className="fixed top-2 left-0 right-0 mx-[12px] 
                     bg-black/10 backdrop-blur-md text-white font-semibold 
                     rounded-lg transition-all duration-700 ease-in-out z-10 h-[49px] align-middle pt-1">
       <div className="max-w-[1390px] mx-auto flex justify-between items-center px-6 py-2 align-middle">
-        <Link href="/" className="text-2xl font-bold" >
+        <a href="/" className="text-2xl font-bold" >
           camelAI
-        </Link>
+        </a>
 
         <div className="space-x-6 align-middle">
-          <Link href="/blog" className="nav-hover transition-colors duration-300 relative pb-1">
+          <a href="/blog" className="nav-hover transition-colors duration-300 relative pb-1">
             Blog
-          </Link>
-          <Link href="/Pricing" className="nav-hover transition-colors duration-300 relative pb-1">
+          </a>
+          <a href="/Pricing" className="nav-hover transition-colors duration-300 relative pb-1">
             Pricing
-          </Link>
-          <Link href="/login" className="nav-hover transition-colors duration-300 relative pb-1">
+          </a>
+          <a href="/login" className="nav-hover transition-colors duration-300 relative pb-1">
             Log In
-          </Link>
+          </a>
         </div>
       </div>
     </nav>
