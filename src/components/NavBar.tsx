5ef69e293516c97db390ee4cd13693528400d12e
Artyom WSL <artyomgv05@gmail.com>
2025-03-01 22:36:26 -0500

    deployment test

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
 
@@ -8,7 +10,7 @@ export default function Navbar() {
                     rounded-lg transition-all duration-700 ease-in-out z-10 h-[49px] align-middle pt-1">
       <div className="max-w-[1390px] mx-auto flex justify-between items-center px-6 py-2 align-middle">
         <Link href="/" className="text-2xl font-bold" >
-          <Image src="https://camelai.com/assets/images/main-camelai.png" alt="camelAI logo"  width={96} height={10}></Image>
+          camelAI
         </Link>
 
         <div className="space-x-6 align-middle">
