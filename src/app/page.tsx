9f98e2ac1f9f2eb8da974290095292082e8357e9
Artyom WSL <artyomgv05@gmail.com>
2025-03-01 22:32:14 -0500

    deployment test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,30 @@ import SectionImage from "@/components/SectionImage";
 import SectionColumn from "@/components/SectionColumn";
 import SectionFAQ from "@/components/SectionFAQ";
 import SectionGetStarted from "@/components/SectionGetStarted";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function Home() {
     return (
-        <main className="justify-center justify-items-center align-middle">
-            <SectionMain/>
-            <SectionCarousel/>
-            <SectionImage h1="Ask any question" h2_1="Camel uses your live data." h2_2="Get tables and charts instantly."
-                bannerSrc="https://camelai.com/assets/images/chat-demo-ss.png"
-                iconSrc="https://camelai.com/assets/images/code-icon.png">
-            </SectionImage>
-            <SectionImage h1="Powerful Dashboards" h2_1="Save any graph to a live dashboard." h2_2="Start a chat to answer ad hoc questions.
-preview ."
-                bannerSrc="https://camelai.com/assets/images/dashboard-demo-ss.png"
-                iconSrc="https://camelai.com/assets/images/light-bulb-icon.png">
-            </SectionImage>
-            <SectionColumn/>
-            <SectionFAQ/>
-            <SectionGetStarted/>
-        </main>
+        <div>
+            <Navbar></Navbar>
+            <main className="justify-center justify-items-center align-middle">
+                <SectionMain/>
+                <SectionCarousel/>
+                <SectionImage h1="Ask any question" h2_1="Camel uses your live data." h2_2="Get tables and charts instantly."
+                    bannerSrc="https://camelai.com/assets/images/chat-demo-ss.png"
+                    iconSrc="https://camelai.com/assets/images/code-icon.png">
+                </SectionImage>
+                <SectionImage h1="Powerful Dashboards" h2_1="Save any graph to a live dashboard." h2_2="Start a chat to answer ad hoc questions.
+    preview ."
+                    bannerSrc="https://camelai.com/assets/images/dashboard-demo-ss.png"
+                    iconSrc="https://camelai.com/assets/images/light-bulb-icon.png">
+                </SectionImage>
+                <SectionColumn/>
+                <SectionFAQ/>
+                <SectionGetStarted/>
+            </main>
+            <Footer></Footer>
+        </div>
     );
 }
