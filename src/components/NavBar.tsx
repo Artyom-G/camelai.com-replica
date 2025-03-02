d9d9e0ca3ecfad87816c2257a6a730ddaf1f30a3
Artyom WSL <artyomgv05@gmail.com>
2025-03-01 22:43:18 -0500

    deploying navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,7 +2,7 @@
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-2 left-0 right-0 mx-[12px] 
+    <div className="fixed top-2 left-0 right-0 mx-[12px] 
                     bg-black/10 backdrop-blur-md text-white font-semibold 
                     rounded-lg transition-all duration-700 ease-in-out z-10 h-[49px] align-middle pt-1">
       <div className="max-w-[1390px] mx-auto flex justify-between items-center px-6 py-2 align-middle">
@@ -22,6 +22,6 @@ export default function Navbar() {
           </a>
         </div>
       </div>
-    </nav>
+    </div>
   );
 }
